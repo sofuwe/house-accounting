e757d9cc2844b6b5cba6cc61f51e61c636536dd3
Imran Ariffin <ariffin.imran@gmail.com>
2022-12-27 11:58:30 -0500

    (#1) Import all transactions files in a directory (#2)

--- imported/management/commands/import_transactions.py
+++ imported/management/commands/import_transactions.py
@@ -1,3 +1,6 @@
+import logging
+import os
+
 from django.core.management.base import BaseCommand
 
 from typing import TYPE_CHECKING
@@ -11,6 +14,8 @@ if TYPE_CHECKING:  # pragma: no cover
     from django.core.management.base import CommandParser
 
     from ...interfaces import ITransactionImporter
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -40,9 +45,44 @@ class Command(BaseCommand):
 
     def handle(self, *args: "Any", **options: "Any") -> "Optional[str]":
         source: str = options["source"]
-        institution: TransactionInstitutionSource = TransactionInstitutionSource[options["institution"]]
+        institution: TransactionInstitutionSource = TransactionInstitutionSource[
+            options["institution"]
+        ]
+        if os.path.isfile(source):
+            self._import_from_file(source=source, institution=institution)
+        else:
+            self._import_from_directory(source_dir=source, institution=institution)
+
+    def _import_from_file(
+        self, 
+        source: str, 
+        institution: TransactionInstitutionSource,
+    ) -> None:
+        logger.info("Import from file %s", source)
         importer: "ITransactionImporter" = Importer.from_args(
             source=source,
             institution=institution,
-    )
+        )
         importer.process()
+
+    def _import_from_directory(
+        self,
+        source_dir: str,
+        institution: TransactionInstitutionSource,
+    ) -> None:
+        for source_file_or_dir in os.listdir(source_dir):
+            source_file_or_dir: str = os.path.join(
+                source_dir,
+                source_file_or_dir,
+            )
+            if os.path.isdir(source_file_or_dir):
+                logger.info("Skip %s since it is a directory", source_file_or_dir)
+                continue
+
+            source_file = source_file_or_dir
+            logger.info("Import from file %s", source_file)
+            importer: "ITransactionImporter" = Importer.from_args(
+                source=source_file,
+                institution=institution,
+            )
+            importer.process()
