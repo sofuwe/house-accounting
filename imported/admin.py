a4ff6ad8bed8ca18191e3e1bf62496e77bb9ac79
Imran Ariffin <ariffin.imran@gmail.com>
2022-12-27 19:58:23 -0500

    (#3) Add admin page for imported transactions

--- imported/admin.py
+++ imported/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Transaction
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    fields = (
+        "transaction_id",
+        "transaction_id_raw",
+        "date",
+        "amount",
+    )
+    list_display = (
+        "transaction_id_raw",
+        "date",
+        "amount",
+    )
+    ordering = ("-date",)
